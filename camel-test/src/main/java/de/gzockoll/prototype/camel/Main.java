38abd6b76937ffe1c0ffccf83841aa7c8cffef36
Guido Zockoll <Guido Zockoll@payone.de>
2011-11-16 13:34:27 +0100

    added menu to frame

--- camel-test/src/main/java/de/gzockoll/prototype/camel/Main.java
+++ camel-test/src/main/java/de/gzockoll/prototype/camel/Main.java
@@ -1,51 +1,97 @@
 package de.gzockoll.prototype.camel;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 
+@SuppressWarnings("javadoc")
 public class Main {
+
+    private class WindowEventHandler extends WindowAdapter {
+        private final CamelContext context;
+
+        public WindowEventHandler(CamelContext context) {
+            this.context = context;
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            super.windowClosing(e);
+            onExit();
+        }
+    }
 
     /**
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        CamelContext context = new DefaultCamelContext();
-    	context.getManagementStrategy().getManagementAgent().setCreateConnector(true);
+        new Main().run();
+    }
+
+    private CamelContext context;
+
+    public void run() throws Exception {
+        context = new DefaultCamelContext();
+        context.getManagementStrategy().getManagementAgent().setCreateConnector(true);
 
         context.addRoutes(new MyRouteBuilder());
 
         context.setTracing(true);
         context.start();
-        JFrame frame=new JFrame();
+        showFrame();
+    }
+
+    /**
+     *
+     */
+    private void onExit() {
+        try {
+            context.stop();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            System.exit(0);
+        }
+    }
+
+    /**
+     *
+     */
+    private void showFrame() {
+        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.addWindowListener(new WindowEventHandler(context));
+
+        JMenu file = new JMenu("File");
+        file.setMnemonic('F');
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.setMnemonic('x');
+        file.add(exitItem);
+
+        // adding action listener to menu items
+        exitItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onExit();
+            }
+        });
+
+        JMenuBar bar = new JMenuBar();
+        frame.setJMenuBar(bar);
+        bar.add(file);
+
+        frame.setSize(200, 200);
         frame.pack();
         frame.setVisible(true);
-    }
-    
-    private static class WindowEventHandler extends WindowAdapter {
-    	private CamelContext context;
-
-		public WindowEventHandler(CamelContext context) {
-			this.context=context;
-		}
-
-		@Override
-    	public void windowClosing(WindowEvent e) {
-    		super.windowClosing(e);
-            try {
-				context.stop();
-			} catch (Exception ex) {
-				throw new RuntimeException(ex);
-			} finally {
-				System.exit(0);
-			}
-    	}
     }
 }
--- camel-test/src/main/java/de/gzockoll/prototype/camel/MyRouteBuilder.java
+++ camel-test/src/main/java/de/gzockoll/prototype/camel/MyRouteBuilder.java
@@ -2,19 +2,28 @@ package de.gzockoll.prototype.camel;
 
 import java.util.List;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.BindyType;
 
 final class MyRouteBuilder extends RouteBuilder {
     @Override
     public void configure() {
-        // errorHandler(deadLetterChannel("log:dead?level=ERROR"));
-        errorHandler(defaultErrorHandler().maximumRedeliveries(3).redeliveryDelay(3000));
-        from("file:data/inbox/?fileName=data.csv&noop=true")
-                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").unmarshal()
-                .bindy(BindyType.Csv, "de.gzockoll.prototype.camel.bindy")
-                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").split(body(List.class))
-                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").convertBodyTo(Ueberweisung.class)
-                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").to("mock:data");
+        from("file:data/inbox/?noop=true&delay=10000")
+                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").multicast().to("seda:process")
+                .to("seda:audit").end();
+
+        from("seda:process")
+                .errorHandler(
+                        deadLetterChannel("seda:error").maximumRedeliveries(5)
+                                .retryAttemptedLogLevel(LoggingLevel.INFO).redeliveryDelay(250).backOffMultiplier(2))
+                .unmarshal().bindy(BindyType.Csv, "de.gzockoll.prototype.camel.bindy").split(body(List.class))
+                .convertBodyTo(Ueberweisung.class).to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true")
+                .to("mock:data");
+
+        from("seda:audit?concurrentConsumers=5").to("file:data/audit/");
+        from("seda:error").marshal().xstream("UTF-8")
+        // .setHeader(Exchange.FILE_NAME, "${file:name.noext}-${date:now:yyyyMMddHHmmssSSS}.xml")
+                .to("file:data/error");
     }
 }

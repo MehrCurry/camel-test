e45c0ae76e1b2e3463f94c6e558cfe27cd160a52
Guido Zockoll <gzockoll@gmail.com>
2011-11-16 07:09:09 +0100

    added Swing Window

--- camel-test/src/main/java/de/gzockoll/prototype/camel/Main.java
+++ camel-test/src/main/java/de/gzockoll/prototype/camel/Main.java
@@ -1,5 +1,10 @@
 package de.gzockoll.prototype.camel;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -11,12 +16,36 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
         CamelContext context = new DefaultCamelContext();
+    	context.getManagementStrategy().getManagementAgent().setCreateConnector(true);
 
         context.addRoutes(new MyRouteBuilder());
 
         context.setTracing(true);
         context.start();
-        Thread.sleep(8000);
-        context.stop();
+        JFrame frame=new JFrame();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.addWindowListener(new WindowEventHandler(context));
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    private static class WindowEventHandler extends WindowAdapter {
+    	private CamelContext context;
+
+		public WindowEventHandler(CamelContext context) {
+			this.context=context;
+		}
+
+		@Override
+    	public void windowClosing(WindowEvent e) {
+    		super.windowClosing(e);
+            try {
+				context.stop();
+			} catch (Exception ex) {
+				throw new RuntimeException(ex);
+			} finally {
+				System.exit(0);
+			}
+    	}
     }
 }

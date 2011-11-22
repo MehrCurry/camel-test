9cc507453e7b44cfa8d8fc44e1d036b184d38282
Guido Zockoll <Guido Zockoll@payone.de>
2011-11-22 16:33:07 +0100

    route cleanup

--- src/main/java/de/gzockoll/prototype/camel/MyRouteBuilder.java
+++ src/main/java/de/gzockoll/prototype/camel/MyRouteBuilder.java
@@ -10,16 +10,14 @@ import org.apache.camel.model.dataformat.BindyType;
 final class MyRouteBuilder extends RouteBuilder {
     @Override
     public void configure() {
-        from("file:data/inbox/?noop=true&delay=10000").to("log:de.gzockoll.prototype.camel").multicast()
-                .to("seda:input");
+        from("file:data/inbox/?noop=true&delay=10000").to("seda:input");
 
-        from("ftp://ftpuser@zockoll.dyndns.org/in?password=tux88.").to("log:de.gzockoll.prototype.camel").multicast()
-                .to("seda:input");
+        from("ftp://ftpuser@zockoll.dyndns.org/in?password=tux88.").to("seda:input");
 
         from("seda:input").to("log:de.gzockoll.prototype.camel").multicast().to("seda:process").to("seda:audit").end();
 
         from("seda:process")
-                // .errorHandler(deadLetterChannel("seda:error"))
+                .errorHandler(deadLetterChannel("seda:error"))
                 // .maximumRedeliveries(5).retryAttemptedLogLevel(LoggingLevel.INFO).redeliveryDelay(250).backOffMultiplier(2))
                 .unmarshal().bindy(BindyType.Csv, "de.gzockoll.prototype.camel.bindy").split(body(List.class))
                 .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").convertBodyTo(Ueberweisung.class)
@@ -27,6 +25,7 @@ final class MyRouteBuilder extends RouteBuilder {
                 .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").to("mock:data");
 
         from("seda:audit?concurrentConsumers=5").to("file:data/audit/");
+
         from("seda:error")
                 .marshal()
                 .xstream("UTF-8")

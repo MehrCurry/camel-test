ef759921ce1d0c7531d45e43fcdb252a002d3f91
Guido Zockoll <Guido Zockoll@payone.de>
2011-11-22 16:26:24 +0100

    booking

--- src/main/java/de/gzockoll/prototype/camel/MyRouteBuilder.java
+++ src/main/java/de/gzockoll/prototype/camel/MyRouteBuilder.java
@@ -3,7 +3,6 @@ package de.gzockoll.prototype.camel;
 import java.util.List;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.BindyType;
 
@@ -12,17 +11,20 @@ final class MyRouteBuilder extends RouteBuilder {
     @Override
     public void configure() {
         from("file:data/inbox/?noop=true&delay=10000").to("log:de.gzockoll.prototype.camel").multicast()
-                .to("seda:process").to("seda:audit").end();
+                .to("seda:input");
 
         from("ftp://ftpuser@zockoll.dyndns.org/in?password=tux88.").to("log:de.gzockoll.prototype.camel").multicast()
-                .to("seda:process").to("seda:audit").end();
+                .to("seda:input");
+
+        from("seda:input").to("log:de.gzockoll.prototype.camel").multicast().to("seda:process").to("seda:audit").end();
 
         from("seda:process")
-                .errorHandler(
-                        deadLetterChannel("seda:error").maximumRedeliveries(5)
-                                .retryAttemptedLogLevel(LoggingLevel.INFO).redeliveryDelay(250).backOffMultiplier(2))
+                // .errorHandler(deadLetterChannel("seda:error"))
+                // .maximumRedeliveries(5).retryAttemptedLogLevel(LoggingLevel.INFO).redeliveryDelay(250).backOffMultiplier(2))
                 .unmarshal().bindy(BindyType.Csv, "de.gzockoll.prototype.camel.bindy").split(body(List.class))
-                .convertBodyTo(Ueberweisung.class).to("log:de.gzockoll.prototype.camel").to("mock:data");
+                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").convertBodyTo(Ueberweisung.class)
+                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").process(new AccountingProcessor())
+                .to("log:de.gzockoll.prototype.camel?showAll=true&multiline=true").to("mock:data");
 
         from("seda:audit?concurrentConsumers=5").to("file:data/audit/");
         from("seda:error")
